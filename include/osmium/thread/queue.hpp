3230b931ede599596c1e80f4bc44c9fe0a7f8636
Jochen Topf <jochen@topf.org>
2016-04-01 11:40:08 +0200

    Function wait_and_pop_with_timeout() is not needed any more.

--- include/osmium/thread/queue.hpp
+++ include/osmium/thread/queue.hpp
@@ -148,19 +148,6 @@ namespace osmium {
                 }
             }
 
-            void wait_and_pop_with_timeout(T& value) {
-                std::unique_lock<std::mutex> lock(m_mutex);
-                if (!m_data_available.wait_for(lock, std::chrono::seconds(1), [this] {
-                    return !m_queue.empty();
-                })) {
-                    return;
-                }
-                if (!m_queue.empty()) {
-                    value = std::move(m_queue.front());
-                    m_queue.pop();
-                }
-            }
-
             bool try_pop(T& value) {
                 std::lock_guard<std::mutex> lock(m_mutex);
                 if (m_queue.empty()) {
